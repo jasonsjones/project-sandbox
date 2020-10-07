0ddaa4928036994739c2219c6b050781160802e6
Jason S. Jones <jsjones96@gmail.com>
2020-10-06 21:29:04 -0700

    feat: add shared lib package

--- packages/api/src/__tests__/status.test.ts
+++ packages/api/src/__tests__/status.test.ts
@@ -1,6 +1,7 @@
 import { Application } from 'express';
 import createApp from '../config/app';
 import { makeGraphQLCall } from '../testutils';
+import { statusQuery } from '@orion/shared';
 
 let app: Application;
 
@@ -10,9 +11,7 @@ beforeAll(async () => {
 
 describe('query to verify server status', () => {
     it('it works', () => {
-        const query = `query { status }`;
-
-        return makeGraphQLCall(app, query).then((res) => {
+        return makeGraphQLCall(app, statusQuery).then((res) => {
             expect(res.body.data).toHaveProperty('status');
             expect(res.body.data.status).toEqual('Graphql api is working');
         });
