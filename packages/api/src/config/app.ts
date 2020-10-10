d71a0d09c8eb540ac09d25d6002989df68b49146
Jason S. Jones <jsjones96@gmail.com>
2020-10-09 17:40:38 -0700

    feat: add simple api integration to check graphql status

--- packages/api/src/config/app.ts
+++ packages/api/src/config/app.ts
@@ -1,5 +1,14 @@
-import express, { Application, Request, Response } from 'express';
+import express, { Application, NextFunction, Request, Response } from 'express';
 import { createGraphqlServer } from './graphqlServer';
+
+const clientUrls = ['http://localhost:4200'];
+
+function cors(_: Request, res: Response, next: NextFunction): void {
+    res.append('Access-Control-Allow-Origin', clientUrls);
+    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.append('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
 
 function handleRootAPIRoute(_: Request, res: Response): Response {
     return res.json({
@@ -11,6 +20,8 @@ function handleRootAPIRoute(_: Request, res: Response): Response {
 async function createApp(): Promise<Application> {
     const app = express();
     const server = await createGraphqlServer();
+
+    app.use(cors);
 
     app.get('/api', handleRootAPIRoute);
 
