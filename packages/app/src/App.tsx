2c5b9e3381c2b4e5d0fbb17f8a64f704698785e1
Jason S. Jones <jsjones96@gmail.com>
2020-10-15 18:25:17 -0700

    feat: add simple feature cards as placeholders

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react';
+import secureLogo from './assets/secure.svg';
+import innovativeLogo from './assets/innovative.svg';
 
 function App(): JSX.Element {
     const [status, setStatus] = useState(false);
@@ -12,6 +14,10 @@ function App(): JSX.Element {
             .then((res) => res.json())
             .then(({ data }) => {
                 setStatus(!!data);
+                setIsLoading(false);
+            })
+            .catch((err) => {
+                console.log(err);
                 setIsLoading(false);
             });
     }, []);
@@ -63,6 +69,47 @@ function App(): JSX.Element {
                     {status ? 'All Good!' : 'Ah snap... 😔 '}
                 </div>
             )}
+
+            <div className="w-full mx-auto mt-12 px-8 bg-gray-100">
+                <h2 className="text-3xl text-purple-900 text-center py-6">Features</h2>
+                <div className="flex flex-col justify-around pb-6 lg:flex-row">
+                    <div className="flex flex-col justify-center lg:w-1/2">
+                        <h3 className="text-2xl text-gray-800 text-center">Secure</h3>
+                        <div className="flex flex-col p-4 lg:flex-row">
+                            <img
+                                src={secureLogo}
+                                width="200"
+                                alt="secure illustration"
+                                className="self-center lg:self-start"
+                            />
+                            <p className="mt-6 text-gray-600 lg:ml-4 lg:mt-0">
+                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci
+                                illum laborum libero maxime mollitia, amet consequuntur odit id?
+                                Neque quo quaerat numquam minima blanditiis? Inventore, et quos?
+                                Optio, nesciunt maiores.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className="flex flex-col justify-center mt-8 lg:mt-0 lg:w-1/2">
+                        <h3 className="text-2xl text-gray-800 text-center">Innovative</h3>
+                        <div className="flex flex-col p-4 lg:flex-row">
+                            <img
+                                src={innovativeLogo}
+                                width="200"
+                                alt="innovative illustration"
+                                className="self-center lg:self-start"
+                            />
+                            <p className="mt-6 text-gray-600 lg:ml-4 lg:mt-0">
+                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci
+                                illum laborum libero maxime mollitia, amet consequuntur odit id?
+                                Neque quo quaerat numquam minima blanditiis? Inventore, et quos?
+                                Optio, nesciunt maiores.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
