407d994fdf826f464e10c032b1d50de88b1fca21
Jason S. Jones <jsjones96@gmail.com>
2020-10-16 08:25:17 -0700

    test: update app tests to mock graphql requests

--- packages/app/src/App.test.tsx
+++ packages/app/src/App.test.tsx
@@ -1,37 +1,38 @@
-import { enableFetchMocks } from 'jest-fetch-mock';
 import React from 'react';
 import { render, waitFor } from '@testing-library/react';
+import { mocked } from 'ts-jest/utils';
 import '@testing-library/jest-dom/extend-expect';
 import App from './App';
-enableFetchMocks();
+import { makeGraphQLQuery } from './dataservice';
 
-beforeEach(() => {
-    fetchMock.resetMocks();
-});
+jest.mock('./dataservice');
 
 test('renders hello react text', async () => {
-    fetchMock.mockResponseOnce(JSON.stringify({ data: { status: 'Test is working' } }));
+    mocked(makeGraphQLQuery).mockImplementation(() =>
+        Promise.resolve({ data: { status: 'Test is working!' } })
+    );
 
     const { getByText } = render(<App />);
     const textElement = await waitFor(() => getByText(/hello react/i));
     expect(textElement).toBeInTheDocument();
-    expect(fetchMock).toHaveBeenCalled();
 });
 
 test(`renders 'All Good!'' when graphql call is successful`, async () => {
-    fetchMock.mockResponseOnce(JSON.stringify({ data: { status: 'Test is working' } }));
+    mocked(makeGraphQLQuery).mockImplementation(() =>
+        Promise.resolve({ data: { status: 'Mock is working!' } })
+    );
 
     const { getByText } = render(<App />);
     const textElement = await waitFor(() => getByText(/all good/i));
     expect(textElement).toBeInTheDocument();
-    expect(fetchMock).toHaveBeenCalled();
 });
 
-test.skip(`renders 'Ah snap!'' when something is wrong with graphql call`, async () => {
-    fetchMock.mockResponseOnce(JSON.stringify({ error: { status: 'Something went wrong...' } }));
+test(`renders 'Ah snap!'' when something is wrong with graphql call`, async () => {
+    mocked(makeGraphQLQuery).mockImplementation(() =>
+        Promise.resolve({ error: { message: 'Something went wrong.' } })
+    );
 
     const { getByText } = render(<App />);
     const textElement = await waitFor(() => getByText(/ah snap/i));
     expect(textElement).toBeInTheDocument();
-    expect(fetchMock).toHaveBeenCalled();
 });
