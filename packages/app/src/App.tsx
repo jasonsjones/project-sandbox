5689a58a43018971c5afcdf8519add0c4bb7d231
Jason S. Jones <jsjones96@gmail.com>
2020-10-20 10:07:40 -0700

    feat: add file upload component

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import {
     QueryCache,
     ReactQueryCacheProvider,
@@ -334,6 +334,130 @@ function Features(): JSX.Element {
 
 // #endregion
 
+// #region File upload *******
+
+function FileUpload(): JSX.Element {
+    const imgRef = useRef<HTMLImageElement>(null);
+    const [highlight, setHighlight] = useState(false);
+    const [image, setImage] = useState<File | null>(null);
+    const [error, setError] = useState('');
+
+    const handleDragEnter: React.DragEventHandler<HTMLDivElement> = (e) => {
+        setHighlight(true);
+    };
+    const handleDragLeave: React.DragEventHandler<HTMLDivElement> = (e) => {
+        setHighlight(false);
+    };
+    const handleDragOver: React.DragEventHandler<HTMLDivElement> = (e) => {
+        e.preventDefault();
+    };
+
+    const handleDrop: React.DragEventHandler<HTMLDivElement> = (e) => {
+        e.preventDefault();
+        setHighlight(false);
+        const file = e.dataTransfer.files[0];
+        if (file) {
+            processFile(file);
+        }
+    };
+
+    const handleChooseFile: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+        if (e.target && e.target.files) {
+            processFile(e.target.files[0]);
+        }
+    };
+
+    const processFile = (file: File): void => {
+        if (file.type === 'image/png') {
+            setImage(file);
+            setError('');
+            const reader = new FileReader();
+            reader.onloadend = function () {
+                if (imgRef.current) {
+                    imgRef.current.src = reader.result as string;
+                }
+            };
+            if (file) {
+                reader.readAsDataURL(file);
+            } else {
+                if (imgRef.current) {
+                    imgRef.current.src = '';
+                }
+            }
+        } else {
+            setError('Invalid file extension');
+        }
+    };
+
+    const clearFile = (): void => {
+        setImage(null);
+    };
+
+    return (
+        <div className="text-gray-700">
+            <h3 className="text-center text-2xl mb-2">Upload Image</h3>
+            <p className="italic text-center mb-2">Note: No API support yet...</p>
+            {!image && (
+                <div>
+                    <div
+                        className={`p-6 border-2 border-dashed rounded-lg text-center ${
+                            highlight ? 'border-green-600 bg-green-100' : 'border-gray-400'
+                        }`}
+                        onDragEnter={handleDragEnter}
+                        onDragLeave={handleDragLeave}
+                        onDragOver={handleDragOver}
+                        onDrop={handleDrop}
+                    >
+                        Drop Zone <em>(.png only)</em>
+                    </div>
+                    {error && <p className="mt-2 text-red-700">Error: {error}</p>}
+                    <div className="mt-8">
+                        <label
+                            htmlFor="file-upload"
+                            className="border-2 rounded-lg p-4 cursor-pointer"
+                        >
+                            Choose File
+                        </label>
+                        <input
+                            type="file"
+                            id="file-upload"
+                            className="hidden"
+                            accept="image/png"
+                            onChange={handleChooseFile}
+                        />
+                    </div>
+                </div>
+            )}
+            {image && (
+                <div>
+                    <div className="mt-4 p-4 flex border-2 rounded-lg">
+                        <img
+                            src=""
+                            className="h-24 w-24 rounded-full"
+                            alt="Avatar preview"
+                            ref={imgRef}
+                        />
+                        <div className="ml-8 pt-4 text-gray-700 flex justify-between w-full">
+                            <span className="">{image.name}</span>
+                            <span>{image.size / 1000} Kb</span>
+                        </div>
+                    </div>
+                    <div className="flex justify-end">
+                        <Button className="my-4 mr-6" variant="secondary" clickAction={clearFile}>
+                            Cancel
+                        </Button>
+                        <Button type="button" className="my-4" variant="primary">
+                            Upload
+                        </Button>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+}
+
+// #endregion
+
 // #region App *******
 
 function App(): JSX.Element {
@@ -351,6 +475,10 @@ function App(): JSX.Element {
 
             <UserRegisterForm className="w-3/4 mx-auto mt-12 md:w-1/3" />
             <UserList className="w-full mx-auto mt-12 py-6 md:w-3/4 md:py-0" />
+
+            <div className="my-4 mx-auto max-w-md">
+                <FileUpload />
+            </div>
 
             <ReactQueryDevtools initialIsOpen />
         </ReactQueryCacheProvider>
