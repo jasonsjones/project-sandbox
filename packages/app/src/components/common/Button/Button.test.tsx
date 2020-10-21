031150e3e95d472df627a610db56f996541472f6
Jason S. Jones <jsjones96@gmail.com>
2020-10-21 08:00:04 -0700

    test: add tests for Button component

--- packages/app/src/components/common/Button/Button.test.tsx
+++ packages/app/src/components/common/Button/Button.test.tsx
@@ -4,17 +4,29 @@ import user from '@testing-library/user-event';
 import Button from './Button';
 
 describe('Button component', () => {
-    it('renders a <button>', () => {
-        const { container } = render(<Button>Test Button</Button>);
-        expect(container.firstChild?.nodeName).toEqual('BUTTON');
+    const BUTTON_TEXT = 'Test Button';
+
+    it('renders a single <button>', () => {
+        const { container } = render(<Button>{BUTTON_TEXT}</Button>);
+        expect(container.querySelectorAll('button')).toHaveLength(1);
+    });
+
+    it('renders a default <button>', () => {
+        const { container } = render(<Button></Button>);
+        const button = container.querySelector('button[type="button"]');
+        expect(button).toBeTruthy();
+    });
+
+    it('secondary variant has a subtle bg color', () => {
+        const { getByText } = render(<Button variant="secondary">{BUTTON_TEXT}</Button>);
+        const button = getByText(BUTTON_TEXT);
+        expect(button.classList.contains('bg-gray-100')).toBe(true);
     });
 
     it('fires click handler when clicked', () => {
-        expect.assertions(1);
-
         const handler = jest.fn();
-        const { getByText } = render(<Button clickAction={handler}>Test Button</Button>);
-        const button = getByText('Test Button');
+        const { getByText } = render(<Button clickAction={handler}>{BUTTON_TEXT}</Button>);
+        const button = getByText(BUTTON_TEXT);
         user.click(button);
         expect(handler).toHaveBeenCalled();
     });
