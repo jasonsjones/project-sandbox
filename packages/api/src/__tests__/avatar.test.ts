8aade2c7776c77f270fb36defb3247c0263f82dc
Jason S. Jones <jsjones96@gmail.com>
2020-11-02 19:34:15 -0800

    chore: remove unnecessary import

--- packages/api/src/__tests__/avatar.test.ts
+++ packages/api/src/__tests__/avatar.test.ts
@@ -1,5 +1,4 @@
 import fs from 'fs';
-import request from 'supertest';
 import { Application } from 'express';
 import createApp from '../config/app';
 import { makeGraphQLFileUpload } from '../testutils';
