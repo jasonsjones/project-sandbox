4550a73e2edd77be40c7d1c85a13a4d2692f0d2f
Jason S. Jones <jsjones96@gmail.com>
2020-11-10 21:24:17 -0800

    chore: clean up dataservice.ts

--- packages/app/src/dataservice.ts
+++ packages/app/src/dataservice.ts
@@ -27,20 +27,9 @@ export const makeGraphQLMutation: MutationFunction<GraphQLResponse, QueryPayload
     query,
     variables
 }): Promise<GraphQLResponse> => {
-    // return Promise.resolve({ data: {}, error: {} });
     return fetch(graphqlEndpoint, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ query, variables })
     }).then((res) => res.json());
 };
-
-/*
-export function makeGraphQLMutation(query: string, variables: UserData): Promise<GraphQLResponse> {
-    return fetch(graphqlEndpoint, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ query, variables })
-    }).then((res) => res.json());
-}
-*/
