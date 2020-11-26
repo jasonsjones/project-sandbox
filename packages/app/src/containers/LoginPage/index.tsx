00d9e61679c4c42d3b400794300d57dfe8b2f668
Jason S. Jones <jsjones96@gmail.com>
2020-11-25 17:07:43 -0800

    feat: add simple login container page

---
+++ packages/app/src/containers/LoginPage/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function LoginPage(): JSX.Element {
+    return (
+        <div className="mt-8 mx-auto">
+            <h1 className="text-2xl font-semibold text-center text-purple-900 sm:text-3xl">
+                Login
+            </h1>
+        </div>
+    );
+}
+
+export default LoginPage;
