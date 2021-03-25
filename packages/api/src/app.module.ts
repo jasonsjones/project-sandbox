74302312da27df06fb2089330a24975d66f2f0d3
Jason S. Jones <jsjones96@gmail.com>
2021-03-24 17:28:13 -0700

    refactor: move the status feature out from under src/modules to src/

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -5,7 +5,7 @@ import { graphqlUploadExpress } from 'graphql-upload';
 import { LoggerMiddleware } from './common/logger.middleware';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { StatusModule } from './modules/status/status.module';
+import { StatusModule } from './status/status.module';
 import { UserModule } from './modules/user/user.module';
 import { AvatarModule } from './modules/avatar/avatar.module';
 import { AuthModule } from './modules/auth/auth.module';
