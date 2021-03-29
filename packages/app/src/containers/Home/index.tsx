3cfa9a2d458390bc536520f855c8d4c30bd379bb
Jason S. Jones <jsjones96@gmail.com>
2021-03-29 10:35:54 -0700

    style: update hero banner styles and layout

--- packages/app/src/containers/Home/index.tsx
+++ packages/app/src/containers/Home/index.tsx
@@ -3,19 +3,24 @@ import { Link } from 'react-router-dom';
 import Features from '../../components/Features/indes';
 import HeroBanner from '../../components/HeroBanner';
 import ServerStatus from '../../components/ServerStatus';
+import { useAuthContext } from '../../context/AuthContext';
 
 function Home(): JSX.Element {
+    const { token } = useAuthContext();
+
     return (
         <>
-            <div className="mt-8 mx-12">
-                <HeroBanner />
-            </div>
+            <HeroBanner />
 
             <div className="mt-12 mx-auto flex flex-col items-center">
                 <ServerStatus />
-                <div className="mt-10 text-lg text-gray-600 border-2 border-gray-600 rounded-md py-2 px-4 text-center">
-                    <Link to="/users">List Users</Link>
-                </div>
+                {token ? (
+                    <div className="mt-10 text-lg text-gray-600 border-2 border-gray-600 rounded-md py-2 px-4 text-center">
+                        <Link to="/users">List Users</Link>
+                    </div>
+                ) : (
+                    ''
+                )}
             </div>
 
             <Features />
