25164c3f0273aab12d7379a0ba72478aa7bed521
Jason S. Jones <jsjones96@gmail.com>
2021-05-02 12:45:18 -0700

    feat: add built-in logger to api status feature

--- packages/api/src/status/status.resolver.ts
+++ packages/api/src/status/status.resolver.ts
@@ -1,12 +1,16 @@
+import { Logger } from '@nestjs/common';
 import { Query, Resolver } from '@nestjs/graphql';
 import { StatusService } from './status.service';
 
 @Resolver('Status')
 export class StatusResolver {
+    private readonly logger = new Logger(StatusResolver.name);
+
     constructor(private readonly statusService: StatusService) {}
 
     @Query((_returns) => String, { name: 'status' })
     getStatus(): string {
+        this.logger.log('getStatus query');
         return this.statusService.getMessage();
     }
 }
