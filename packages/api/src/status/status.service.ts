87d097e588cac5f081f175c6cb81b3ec2647315c
Jason S. Jones <jsjones96@gmail.com>
2021-05-02 12:45:18 -0700

    feat: add built-in logger to api status feature

--- packages/api/src/status/status.service.ts
+++ packages/api/src/status/status.service.ts
@@ -1,9 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import constants from './constants';
 
 @Injectable()
 export class StatusService {
+    private readonly logger = new Logger(StatusService.name);
+
     getMessage(): string {
+        this.logger.log('Fetching status message');
         return constants.STATUS_MESSAGE;
     }
 }
