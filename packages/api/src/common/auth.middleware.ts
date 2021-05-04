a2d0d423e0e4af8aeca1ef7cd5ed1e21cc89fe10
Jason S. Jones <jsjones96@gmail.com>
2021-05-03 17:09:46 -0700

    feat: add built-in logger to api auth middleware

--- packages/api/src/common/auth.middleware.ts
+++ packages/api/src/common/auth.middleware.ts
@@ -1,4 +1,4 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 import clc from 'cli-color';
 import { AuthService } from '../auth/auth.service';
@@ -6,6 +6,7 @@ import { UserService } from '../user/user.service';
 
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
+    private readonly logger = new Logger(AuthMiddleware.name);
     constructor(private authService: AuthService, private userService: UserService) {}
 
     async use(req: Request, _: Response, next: NextFunction) {
@@ -20,14 +21,17 @@ export class AuthMiddleware implements NestMiddleware {
                     );
 
                     if (authUser) {
+                        this.logger.log('Request from authenticated user');
                         req.user = authUser;
                     }
                 } catch (err) {
                     // noop for any errors...
                     if (process.env.NODE_ENV !== 'test') {
-                        console.log(clc.red('Auth middleware error: ', err.message));
+                        this.logger.error(`Bearer token (jwt) error: ${clc.red(err.message)}`);
                     }
                 }
+            } else {
+                this.logger.log('No bearer token (jwt) provided');
             }
         }
         next();
