a53a43612762e40ce427e621d21c4bbd911dc855
Jason S. Jones <jsjones96@gmail.com>
2021-05-06 17:00:53 -0700

    style: refactor layout of user list page

--- packages/app/src/containers/UserListPage/index.tsx
+++ packages/app/src/containers/UserListPage/index.tsx
@@ -4,12 +4,12 @@ import UserList from '../../components/UserList';
 
 function UserListPage(): JSX.Element {
     return (
-        <>
-            <UserList className="w-full mx-auto mt-12 py-6 md:w-3/4 md:py-0" />
+        <div className="w-full md:w-3/4 mx-auto mt-12 px-4">
+            <UserList />
             <div className="mt-24 mb-4 mx-auto max-w-md">
                 <FileUpload />
             </div>
-        </>
+        </div>
     );
 }
 
