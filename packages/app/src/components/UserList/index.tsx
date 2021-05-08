a1daf6097d27126e051fd1c31dfbc0c26f3f78fb
Jason S. Jones <jsjones96@gmail.com>
2021-05-07 17:51:44 -0700

    refactor: lift data fetching to user list page

--- packages/app/src/components/UserList/index.tsx
+++ packages/app/src/components/UserList/index.tsx
@@ -1,21 +1,16 @@
 import React from 'react';
-import useUsers from '../../hooks/useUsers';
-import Spinner from '../Spinner';
 import UserCard from '../UserCard';
+import { User } from '../../types';
 
-type UserListProps = { className?: string };
+type UserListProps = { className?: string; users: User[] };
 
-function UserList({ className }: UserListProps): JSX.Element {
-    const { data, isLoading } = useUsers();
-
-    if (isLoading) return <Spinner />;
-
+function UserList({ className, users }: UserListProps): JSX.Element {
     return (
         <div className={className}>
-            <h2 className="text-4xl text-gray-600 text-center">User List</h2>
+            <h2 className="text-3xl md:text-4xl text-gray-600 text-center">User List</h2>
 
             <div>
-                {data?.data?.users?.length > 0 ? (
+                {users.length > 0 ? (
                     <div className="flex mb-2 border-l-4 border-white text-gray-500 text-lg">
                         <span className="w-1/2 px-2">Id</span>
                         <span className="w-1/2 px-2">Name</span>
@@ -25,36 +20,32 @@ function UserList({ className }: UserListProps): JSX.Element {
                     <p className="mt-4 text-gray-800"> No users currently registered </p>
                 )}
 
-                {data &&
-                    data.data?.users?.map(
-                        (user: { id: string; displayName: string; email: string }) => {
-                            return (
-                                <div
-                                    key={user.id}
-                                    className="flex mb-2 border-l-4 border-white hover:border-purple-600 cursor-pointer"
-                                >
-                                    <span className="w-1/2 px-2 text-sm text-purple-900 md:text-base">
-                                        {user.id}
-                                    </span>
-                                    <span className="w-1/2 px-2 text-sm text-gray-800 md:text-base">
-                                        {user.displayName}
-                                    </span>
-                                    <span className="w-1/2 px-2 text-sm text-gray-800 md:text-base">
-                                        {user.email}
-                                    </span>
-                                </div>
-                            );
-                        }
-                    )}
+                {users.map((user: User) => {
+                    return (
+                        <div
+                            key={user.id}
+                            className="flex mb-2 border-l-4 border-white hover:border-purple-600 cursor-pointer"
+                        >
+                            <span className="w-1/2 px-2 text-sm text-purple-900 md:text-base">
+                                {user.id}
+                            </span>
+                            <span className="w-1/2 px-2 text-sm text-gray-800 md:text-base">
+                                {user.displayName}
+                            </span>
+                            <span className="w-1/2 px-2 text-sm text-gray-800 md:text-base">
+                                {user.email}
+                            </span>
+                        </div>
+                    );
+                })}
             </div>
-            <h2 className="mt-24 mb-2 text-4xl text-gray-600 text-center">User Cards</h2>
+            <h2 className="mt-24 mb-2 text-3xl md:text-4xl text-gray-600 text-center">
+                User Cards
+            </h2>
             <div className="grid grid-flow-rows grid-col-1 mx-8 lg:grid-cols-2 xl:grid-cols-3 gap-4">
-                {data &&
-                    data.data?.users?.map(
-                        (user: { id: string; displayName: string; email: string }) => {
-                            return <UserCard key={user.id} user={user} />;
-                        }
-                    )}
+                {users.map((user: User) => {
+                    return <UserCard key={user.id} user={user} />;
+                })}
             </div>
         </div>
     );
--- packages/app/src/containers/UserListPage/index.tsx
+++ packages/app/src/containers/UserListPage/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import FileUpload from '../../components/FileUpload';
+import Spinner from '../../components/Spinner';
 import UserList from '../../components/UserList';
+import useUsers from '../../hooks/useUsers';
 
 function UserListPage(): JSX.Element {
+    const { data: response, isLoading } = useUsers();
+
+    if (isLoading) return <Spinner />;
+
     return (
         <div className="w-full md:w-3/4 mx-auto mt-12 px-4">
-            <UserList />
+            <UserList users={response?.data.users} />
             <div className="mt-24 mb-4 mx-auto max-w-md">
                 <FileUpload />
             </div>
