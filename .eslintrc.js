c3549c42ae4443cba6ca475527e3d61429944377
Jason S. Jones <jsjones96@gmail.com>
2021-08-26 12:58:01 -0700

    chore: consolidate eslint and prettier configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,22 @@
 module.exports = {
     parser: '@typescript-eslint/parser',
     parserOptions: {
-        sourceType: 'module'
+        ecmaVersion: 6,
+        sourceType: 'module',
+        ecmaFeatures: {}
     },
-    plugins: ['@typescript-eslint/eslint-plugin'],
-    extends: [
-        'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended',
-        'prettier',
-        'prettier/@typescript-eslint'
-    ],
+    plugins: ['@typescript-eslint'],
+    extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended', 'prettier'],
     env: {
+        browser: true,
         node: true,
-        jest: true
+        jest: true,
+        es6: true
     },
     rules: {
+        'prettier/prettier': ['warn'],
+        'no-console': 'off',
+        semi: 'warn',
         '@typescript-eslint/interface-name-prefix': 'off',
         '@typescript-eslint/explicit-function-return-type': 'off',
         '@typescript-eslint/explicit-module-boundary-types': 'off',
