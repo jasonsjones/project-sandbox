e839b23afa7a67c3cc5a889b5572538ba4c31be5
Jason S. Jones <jsjones96@gmail.com>
2021-08-30 18:51:00 -0700

    feat: add buttons to switch user page view (list or cards)

--- packages/app/src/containers/UserPage/index.tsx
+++ packages/app/src/containers/UserPage/index.tsx
@@ -1,18 +1,49 @@
+import { useState } from 'react';
+import { Button } from '../../components/common';
 import FileUpload from '../../components/FileUpload';
 import Spinner from '../../components/Spinner';
 import UserCards from '../../components/UserCards';
 import UserList from '../../components/UserList';
+import useQueryParams from '../../hooks/useQueryParams';
 import useUsers from '../../hooks/useUsers';
 
+enum VIEW {
+    List = 'list',
+    Cards = 'cards'
+}
+
 function UserPage(): JSX.Element {
+    const query = useQueryParams();
+    const [view, setView] = useState(query.get('view') || VIEW.Cards);
     const { data: response, isLoading } = useUsers();
 
     if (isLoading) return <Spinner />;
 
     return (
         <div className="w-full md:w-3/4 mx-auto mt-12 px-4">
-            <UserList users={response?.data.users} />
-            <UserCards className="mt-24" users={response?.data.users} />
+            <div className="flex justify-center gap-x-4">
+                <Button
+                    type="button"
+                    className="w-32 my-4"
+                    variant={view === VIEW.List ? 'primary' : 'secondary'}
+                    clickAction={() => setView(VIEW.List)}
+                >
+                    List
+                </Button>
+                <Button
+                    type="button"
+                    className="w-32 my-4"
+                    variant={view === VIEW.Cards ? 'primary' : 'secondary'}
+                    clickAction={() => setView(VIEW.Cards)}
+                >
+                    Cards
+                </Button>
+            </div>
+            {view === VIEW.List ? (
+                <UserList className="mt-16" users={response?.data.users} />
+            ) : (
+                <UserCards className="mt-16" users={response?.data.users} />
+            )}
             <div className="mt-24 mb-4 mx-auto max-w-md">
                 <FileUpload />
             </div>
