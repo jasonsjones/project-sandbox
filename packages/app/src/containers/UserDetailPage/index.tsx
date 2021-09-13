a368f4d85913f88c7af4ee59cd2b6b06b446154c
Jason S. Jones <jsjones96@gmail.com>
2021-09-12 20:08:19 -0700

    feat: add custom hook to fetch user by id

--- packages/app/src/containers/UserDetailPage/index.tsx
+++ packages/app/src/containers/UserDetailPage/index.tsx
@@ -1,8 +1,14 @@
 import { useParams } from 'react-router-dom';
 import FileUpload from '../../components/FileUpload';
+import Spinner from '../../components/Spinner';
+import useUser from '../../hooks/useUser';
 
 function UserDetailPage(): JSX.Element {
     const { id } = useParams<{ id: string }>();
+    const { data: response, isLoading } = useUser(id);
+
+    if (isLoading) return <Spinner />;
+
     return (
         <div className="w-full md:w-3/4 mx-auto mt-12 px-4">
             <div className="flex gap-x-8 mt-12">
@@ -13,8 +19,8 @@ function UserDetailPage(): JSX.Element {
                 />
                 <div className="flex flex-col justify-center">
                     <p>{id}</p>
-                    <p className="text-3xl">First Last</p>
-                    <p className="text-xl">user@email.com</p>
+                    <p className="text-3xl">{response?.data?.user.displayName}</p>
+                    <p className="text-xl">{response?.data?.user.email}</p>
                 </div>
             </div>
             <div className="mt-24 mb-4 mx-auto max-w-md">
--- packages/app/src/hooks/useUsers.tsx
+++ packages/app/src/hooks/useUsers.tsx
@@ -5,7 +5,7 @@ import { makeGraphQLQuery } from '../dataservice';
 export default function useUsers() {
     const { token } = useAuthContext();
 
-    const statusQuery = 'query { users { id displayName email } }';
+    const usersQuery = 'query { users { id displayName email } }';
 
-    return useQuery(['users', { query: statusQuery, token }], makeGraphQLQuery);
+    return useQuery(['users', { query: usersQuery, token }], makeGraphQLQuery);
 }
