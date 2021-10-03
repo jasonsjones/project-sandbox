bdf85c3e77481992f5530d68c053e9122e7563b9
Jason S. Jones <jsjones96@gmail.com>
2021-10-02 17:30:00 -0700

    feat: add basic ormconfig file

---
+++ packages/api/ormconfig.ts
@@ -0,0 +1,14 @@
+import { ConnectionOptions } from 'typeorm';
+
+const dbConfig: ConnectionOptions = {
+    type: 'sqlite',
+    database: 'data/sp_dev',
+    synchronize: false,
+    entities: ['dist/src/**/*.entity.js'],
+    migrations: ['dist/src/db/migrations/*.js'],
+    cli: {
+        migrationsDir: 'src/db/migrations'
+    }
+};
+
+export default dbConfig;
