d651a32dc1888bc87cbed03ac8be1bb2b642aeab
Jason Jones <jsjones96@gmail.com>
2022-03-24 12:20:22 -0700

    chore: configure app pkg jest tests with vite

---
+++ packages/app/test/__mocks__/fileTransform.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = {
+    process(_, filename) {
+        return `module.exports = ${JSON.stringify(path.basename(filename))};`;
+    },
+    getCacheKey() {
+        return 'fileTransform';
+    }
+};
+
