121ba486ad2a9a48b6ceebc5c9b18ef26a684fb6
Jason Jones <jsjones96@gmail.com>
2022-03-31 12:23:10 -0700

    feat: add passport-jwt strategy

--- packages/api/src/auth/auth-jwt.guard.ts
+++ packages/api/src/auth/auth-jwt.guard.ts
@@ -3,15 +3,13 @@ import { GqlExecutionContext } from "@nestjs/graphql";
 import { AuthGuard } from "@nestjs/passport";
 
 @Injectable()
-export class GqlAuthGuard extends AuthGuard('local') {
+export class JwtAuthGuard extends AuthGuard('jwt') {
     constructor() {
         super();
     }
 
     getRequest(context: ExecutionContext) {
         const ctx = GqlExecutionContext.create(context);
-        const request = ctx.getContext().req;
-        request.body = ctx.getArgs().loginInput;
-        return request;
+        return ctx.getContext().req;
     }
 }
