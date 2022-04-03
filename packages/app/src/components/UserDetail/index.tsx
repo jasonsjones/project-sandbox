3a5e49dfa8cc01fbeeac4173d0db60508e295a0d
Jason Jones <jsjones96@gmail.com>
2022-04-03 13:17:02 -0700

    feat: add ProfilePic component

--- packages/app/src/components/UserDetail/index.tsx
+++ packages/app/src/components/UserDetail/index.tsx
@@ -1,24 +1,15 @@
-import useAvatar from '../../hooks/useAvatar';
 import { User } from '../../types';
+import ProfilePic from '../ProfilePic';
 
-const DEFAULT_PIC = 'http://localhost:3000/default/avatar.png';
-type UserRecordProps = { className?: string; user: User };
+type UserRecordProps = {
+    className?: string;
+    user: User;
+};
 
 function UserDetail({ user }: UserRecordProps): JSX.Element {
-    let profilePic = DEFAULT_PIC;
-    const { data: response } = useAvatar(user.id);
-
-    if (response?.data.avatar) {
-        profilePic = response.data.avatar;
-    }
-
     return (
         <div className="flex gap-x-8 mt-12">
-            <img
-                src={profilePic}
-                alt="default avatar"
-                className="w-48 rounded-full border-4 border-purple-300"
-            />
+            <ProfilePic id={user.id} size="large" />
             <div className="flex flex-col justify-center gap-y-4">
                 <p className="text-4xl text-purple-800">{user.displayName}</p>
                 <p className="text-2xl text-gray-700">{user.email}</p>
