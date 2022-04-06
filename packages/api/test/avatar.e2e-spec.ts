b6981af08a90717913c847127677656ff5964472
Jason Jones <jsjones96@gmail.com>
2022-04-05 17:53:08 -0700

    test: update avatar api e2e test

--- packages/api/test/avatar.e2e-spec.ts
+++ packages/api/test/avatar.e2e-spec.ts
@@ -1,9 +1,17 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import request from 'supertest';
-import { AvatarModule } from '../src/avatar/avatar.module';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
+import { Test, TestingModule } from '@nestjs/testing';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { graphqlUploadExpress } from 'graphql-upload';
+import request from 'supertest';
+import { AuthModule } from '../src/auth/auth.module';
+import { AuthService } from '../src/auth/auth.service';
+import { AvatarModule } from '../src/avatar/avatar.module';
+import { CreateUserDto } from '../src/user/create-user.dto';
+import { User } from '../src/user/user.entity';
+import { UserModule } from '../src/user/user.module';
+import { UserService } from '../src/user/user.service';
 import { avatarUploadOp } from './utils/constants';
 
 function generateVariableMap(keyName: string) {
@@ -17,12 +25,23 @@ const constants = {
     MAP: 'map'
 };
 
+const barry: CreateUserDto = {
+    firstName: 'Barry',
+    lastName: 'Allen',
+    email: 'barry@starlabs.com',
+    password: 'secretpassword'
+};
+
 describe('Avatar resolver (e2e)', () => {
     let app: INestApplication;
+    let authService: AuthService;
+    let userService: UserService;
 
     beforeEach(async () => {
         const moduleFixture: TestingModule = await Test.createTestingModule({
             imports: [
+                AuthModule,
+                ConfigModule.forRoot(),
                 GraphQLModule.forRoot({
                     autoSchemaFile: 'src/schema.gql',
                     cors: {
@@ -31,17 +50,29 @@ describe('Avatar resolver (e2e)', () => {
                     },
                     context: ({ req, res }) => ({ req, res })
                 }),
-                AvatarModule
+                TypeOrmModule.forRoot({
+                    type: 'sqlite',
+                    database: ':memory:',
+                    synchronize: true,
+                    entities: [User]
+                }),
+                AvatarModule,
+                UserModule
             ]
         }).compile();
 
+        authService = moduleFixture.get<AuthService>(AuthService);
+        userService = moduleFixture.get<UserService>(UserService);
         app = moduleFixture.createNestApplication();
         app.use(graphqlUploadExpress());
         await app.init();
     });
 
     describe('upload avatar mutation', () => {
-        it('uploads png image', () => {
+        it('uploads png image', async () => {
+            const user = await userService.create(barry);
+            const accessToken = authService.generateAccessToken(user);
+
             const testFile = `${__dirname}/../avatars/default/avatar.png`;
 
             const variables = {
@@ -53,6 +84,7 @@ describe('Avatar resolver (e2e)', () => {
             return request(app.getHttpServer())
                 .post('/graphql')
                 .set('Content-Type', 'multipart/form-data')
+                .set('Authorization', `Bearer ${accessToken}`)
                 .field(constants.OPERATIONS, JSON.stringify({ query: avatarUploadOp, variables }))
                 .field(constants.MAP, JSON.stringify(variableMap))
                 .attach(Object.keys(variableMap)[0], testFile)
