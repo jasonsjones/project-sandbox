2704da1401e7aedbe2e98f5e3fb11b1cfd1fa6eb
Jason Jones <jsjones96@gmail.com>
2022-04-11 16:37:27 -0700

    feat: add context user info to auth context

--- packages/app/src/types/index.tsx
+++ packages/app/src/types/index.tsx
@@ -2,6 +2,15 @@ export interface User {
     id: string;
     displayName: string;
     email: string;
+    firstName: string;
+    lastName: string;
+}
+
+export type ContextUserInfo = Pick<User, 'id' | 'displayName' | 'firstName' | 'lastName'>;
+
+export interface LoginResponseData {
+    accessToken: string;
+    userInfo?: ContextUserInfo;
 }
 
 export enum UserListView {
