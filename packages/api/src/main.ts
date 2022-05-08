8950fee8fc902406032fc89dac792e8dced8e322
Jason Jones <jsjones96@gmail.com>
2022-05-07 20:36:02 -0700

    fix: update netlify regex for cors

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors({
-        origin: ['http://localhost:4200', /https:\/\/[a-z0-9]*.netlify.app/],
+        origin: ['http://localhost:4200', /https:\/\/[a-z\-0-9]*.netlify.app/],
         credentials: true
     });
     app.use(cookieParser());
