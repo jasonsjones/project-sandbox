13b29564b3e96d996c356cf984f210cf6d60629b
Jason Jones <jsjones96@gmail.com>
2022-05-09 09:35:44 -0700

    fix: force types/react to v17; add vite-env-plugin

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import EnvironmentPlugin from 'vite-plugin-environment';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     server: {
         port: 4200
     },
-    plugins: [react()]
+    plugins: [react(), EnvironmentPlugin('all')]
 });
